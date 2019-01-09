b11c4ee30023087789befdf19a5e932b9868cc47
naveen-perera <naveen.perera@syscolabs.com>
2019-01-09 07:29:55 +0530

    Using Petfinder API Client

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import React from "react";
+import pf from "petfinder-client";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 class App extends React.Component {
+  componentDidMount() {
+    const promise = petfinder.breed.list({ animal: "dog" });
+    promise.then(console.log, console.error);
+  }
+
   handleTitleClick() {
     alert("You clicked!");
   }
+  // render() {
+  //   // return React.createElement("div", {}, [
+  //   //   React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me"),
+  //   //   React.createElement(Pet, {
+  //   //     name: "Jake",
+  //   //     animal: "Dog",
+  //   //     breed: "GSD"
+  //   //     // onClick: this.handleTitleClick
+  //   //   }),
+  //   //   React.createElement(Pet, {
+  //   //     name: "Pepper",
+  //   //     animal: "Bird",
+  //   //     breed: "Cockatiel"
+  //   //   }),
+  //   //   React.createElement(Pet, {
+  //   //     name: "Doink",
+  //   //     animal: "Cat",
+  //   //     breed: "Mixed"
+  //   //   })
+  //   // ]);
+  // }
   render() {
-    // return React.createElement("div", {}, [
-    //   React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me"),
-    //   React.createElement(Pet, {
-    //     name: "Jake",
-    //     animal: "Dog",
-    //     breed: "GSD"
-    //     // onClick: this.handleTitleClick
-    //   }),
-    //   React.createElement(Pet, {
-    //     name: "Pepper",
-    //     animal: "Bird",
-    //     breed: "Cockatiel"
-    //   }),
-    //   React.createElement(Pet, {
-    //     name: "Doink",
-    //     animal: "Cat",
-    //     breed: "Mixed"
-    //   })
-    // ]);
     return (
       <div>
         <h1 onClick={this.handleTitleClick}>"Adopt Me!"</h1>
