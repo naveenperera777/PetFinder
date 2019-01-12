6139fc0ad1a8f9f1607947fab9142a69f719b442
naveen-perera <naveen.perera@syscolabs.com>
2019-01-12 10:27:48 +0530

    Add Reach router

--- src/App.js
+++ src/App.js
@@ -1,95 +1,19 @@
 import React from "react";
-import pf from "petfinder-client";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import { Router } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
+// import
 
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-
-  componentDidMount() {
-    // const promise = petfinder.breed.list({ animal: "dog" });
-    // promise.then(console.log, console.error);
-
-    petfinder.pet
-      .find({ output: "full", location: "Seattle, WA" })
-      .then(data => {
-        let pets;
-
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-
-        this.setState({
-          pets: pets
-        });
-      });
-  }
-
-  // render() {
-  //   // return React.createElement("div", {}, [
-  //   //   React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me"),
-  //   //   React.createElement(Pet, {
-  //   //     name: "Jake",
-  //   //     animal: "Dog",
-  //   //     breed: "GSD"
-  //   //     // onClick: this.handleTitleClick
-  //   //   }),
-  //   //   React.createElement(Pet, {
-  //   //     name: "Pepper",
-  //   //     animal: "Bird",
-  //   //     breed: "Cockatiel"
-  //   //   }),
-  //   //   React.createElement(Pet, {
-  //   //     name: "Doink",
-  //   //     animal: "Cat",
-  //   //     breed: "Mixed"
-  //   //   })
-  //   // ]);
-  // }
   render() {
     return (
       <div>
-        <h1>"Adopt Me!"</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(". ");
-            } else {
-              breed = pet.breeds.breed;
-            }
-
-            const x = "My dog is" % { breed };
-
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <h1>Adopt Me!</h1>
+        <Router>
+          <Results exact path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
---
+++ src/Results.js
@@ -0,0 +1,95 @@
+import React from "react";
+import pf from "petfinder-client";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    // const promise = petfinder.breed.list({ animal: "dog" });
+    // promise.then(console.log, console.error);
+
+    petfinder.pet
+      .find({ output: "full", location: "New York, NY" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets: pets
+        });
+      });
+  }
+
+  //   render() {
+  //   // return React.createElement("div", {}, [
+  //   //   React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me"),
+  //   //   React.createElement(Pet, {
+  //   //     name: "Jake",
+  //   //     animal: "Dog",
+  //   //     breed: "GSD"
+  //   //     // onClick: this.handleTitleClick
+  //   //   }),
+  //   //   React.createElement(Pet, {
+  //   //     name: "Pepper",
+  //   //     animal: "Bird",
+  //   //     breed: "Cockatiel"
+  //   //   }),
+  //   //   React.createElement(Pet, {
+  //   //     name: "Doink",
+  //   //     animal: "Cat",
+  //   //     breed: "Mixed"
+  //   //   })
+  //   // ]);
+  //   }
+  render() {
+    return (
+      <div className="search">
+        {this.state.pets.map(pet => {
+          let breed;
+
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(". ");
+          } else {
+            breed = pet.breeds.breed;
+          }
+
+          const x = "My dog is" % { breed };
+
+          return (
+            <Pet
+              key={pet.id}
+              animal={pet.animal}
+              name={pet.name}
+              breed={breed}
+              media={pet.media}
+              location={`${pet.contact.city}, ${pet.contact.state}`}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Results;
